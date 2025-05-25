e3e73b90fdea4a3ad857c44917922d97dbd1aeeb
Mike Gelfand <mikedld@mikedld.com>
2025-05-25 21:43:57 +0100

    Add an extra script to run all the others

---
+++ pt/index.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+from subprocess import run
+from sys import stderr
+
+
+SCRIPTS = (
+    "agriloja.py",
+    "auchan.py",
+    "continente.py",
+    "mcdonalds.py",
+    "mercadona.py",
+    "pingodoce.py",
+    "starbucks.py",
+    "worten.py",
+)
+
+
+if __name__ == "__main__":
+    base_dir = Path(__file__).parent
+
+    for script in SCRIPTS:
+        result = run([f"./{script}"], capture_output=True, cwd=base_dir)
+        if result.returncode != 0:
+            print(f"---\nScript '{script}' failed with exit code {result.returncode}: {result.stderr}", file=stderr)
