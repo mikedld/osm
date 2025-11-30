5412513bfc67b6e39645f50993d4823346ae11b1
Mike Gelfand <mikedld@mikedld.com>
2025-11-30 17:28:42 +0000

    Resolve linter findings

--- pt/novobanco.py
+++ pt/novobanco.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python3
 
-import json
-import itertools
 import re
-from multiprocessing import Pool
-import requests
 
-from lxml import etree
+from impl.common import DiffDict, distance, fetch_json_data, overpass_query, titleize, write_diff
 
-from impl.common import DiffDict, fetch_json_data, fetch_html_data, overpass_query, titleize, opening_weekdays, distance, write_diff, DAYS
 
 DATA_URL = "https://srv.novobanco.pt/web/ocb/pesquisabalcoes/balcoesmap"
 
@@ -76,33 +71,29 @@ CITY_IS_IN = {
     "Vilamoura": "Quarteira",
 }
 
-def fetch_data():
-    results = fetch_json_data(DATA_URL, headers={"x-nb-oc-channel": "1.2"})["data"]
 
-    return results
+def fetch_data():
+    return fetch_json_data(DATA_URL, headers={"x-nb-oc-channel": "1.2"})["data"]
+
 
 def fix_postal_code(postal_code):
     return postal_code.replace(" ", "")
+
 
 def fix_city(city):
     if city in CITY_FIXES:
         return CITY_FIXES[city]
     return city
 
+
 if __name__ == "__main__":
     new_data = fetch_data()
-    
-    with open("novobanco.json", "w") as f:
-        json.dump(new_data, f, indent=2, ensure_ascii=False)
-    
-    old_data = [DiffDict(e) for e in overpass_query(
-"""
-(
-    nwr[amenity=bank][name~"Novo Banco"][brand!="Novo Banco dos Açores"](area.country);
-);
-"""
-    )]
-    
+
+    old_data = [
+        DiffDict(e)
+        for e in overpass_query('nwr[amenity=bank][name~"Novo Banco"][brand!="Novo Banco dos Açores"](area.country);')
+    ]
+
     new_node_id = -10000
 
     for nd in new_data:
@@ -124,32 +115,28 @@ if __name__ == "__main__":
             old_data.append(d)
             new_node_id -= 1
 
+        d["amenity"] = "bank"
         if nd["designacao"] == "Sede":
-            d["amenity"] = "bank"
             d["name"] = "Novo Banco - Sede"
-        elif nd["tipoEstrutura"] == "BL" or nd["tipoEstrutura"] == "EXT":
-            d["amenity"] = "bank"
-            if d["name"] is None:
-                d["name"] = "Novo Banco"
+        elif nd["tipoEstrutura"] in ("BL", "EXT"):
+            d["name"] = d["name"] or "Novo Banco"
         elif nd["tipoEstrutura"] == "CE":
-            d["amenity"] = "bank"
-            if d["name"] is None:
-                d["name"] = "Novo Banco Empresas"
+            d["name"] = d["name"] or "Novo Banco Empresas"
         else:
-            raise ValueError(f"Unknown tipoEstrutura: {nd['tipoEstrutura']}")
+            d["name"] = f"<ERR:{nd['tipoEstrutura']}>"
 
         d["operator:wikipedia"] = "pt:Novo Banco"
         d["operator:wikidata"] = "Q17488861"
         d["brand"] = "Novo Banco"
         d["brand:wikipedia"] = "pt:Novo Banco"
         d["brand:wikidata"] = "Q17488861"
-        
+
         if nd["codigo"] not in d[REF].split(";"):
             d[REF] = nd["codigo"]
         ref_name = fix_city(nd["designacao"])
         if ref_name not in d["ref_name"].split(";"):
             d["ref_name"] = ref_name
-        
+
         postcode = fix_postal_code(nd["codigoPostal"])
         if re.match(r"^\d{4}-\d{3}$", postcode):
             d["addr:postcode"] = postcode
@@ -157,18 +144,22 @@ if __name__ == "__main__":
         city = fix_city(titleize(nd["localidadePostal"]))
         if CITY_IS_IN.get(d["addr:city"], None) != city:
             d["addr:city"] = city
-        
+
         phone = "+351 " + nd["telefone"]
         if phone not in d["contact:phone"].split(";"):
             d["contact:phone"] = "+351 " + nd["telefone"]
-        
+
         fax = "+351 " + nd["fax"]
         if fax not in d["contact:fax"].split(";"):
             d["contact:fax"] = "+351 " + nd["fax"]
-                   
-        if d.kind == "new" and not d["addr:street"] and not (d["addr:housenumber"] or d["nohousenumber"] or d["addr:housename"]):
+
+        if (
+            d.kind == "new"
+            and not d["addr:street"]
+            and not (d["addr:housenumber"] or d["nohousenumber"] or d["addr:housename"])
+        ):
             d["x-dld-addr"] = nd["morada"]
-        
+
     for d in old_data:
         if d.kind != "old":
             continue
